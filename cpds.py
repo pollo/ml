b6e2e9a034fe20ca9ac90a026a442f8ebb3fcb47
pollo <pollo@entropy.lan>
2014-11-18 18:11:07 +0100

    first part

---
+++ cpds.py
@@ -0,0 +1,46 @@
+#X -> understands explaining away
+#Y -> looks for new example
+#Z -> having an original solution
+
+X1 = 0.80
+X0 = 1 - X1
+Y1 = 0.70
+Y0 = 1 - Y1
+
+Z00 = 0.05
+Z01 = 0.10
+Z10 = 0.20
+Z11 = 0.99
+
+def Z1():
+    return \
+        X0*Y0*Z00+\
+        X0*Y1*Z01+\
+        X1*Y0*Z10+\
+        X1*Y1*Z11
+
+def Z1_X1():
+    return Y1 * Z11 + Y0 * Z10
+
+def Z1_Y1():
+    return X1 * Z11 + X0 * Z01
+
+def X1_Z1():
+    return X1 * Z1_X1() / Z1()
+
+def X1_Z1Y1():
+    return X1 * Z11 / Z1_Y1()
+
+def Y1_Z1():
+    return Y1 * Z1_Y1() / Z1()
+
+def Y1_Z1X1():
+    return Y1 * Z11 / Z1_X1()
+
+print "P(Z=1) = "+str(Z1())
+print "P(Z=1 | X=1) = "+str(Z1_X1())
+print "P(Z=1 | Y=1) = "+str(Z1_Y1())
+print "P(X=1 | Z=1) = "+str(X1_Z1())
+print "P(X=1 | Z=1,Y=1) = "+str(X1_Z1Y1())
+print "P(Y=1 | Z=1) = "+str(Y1_Z1())
+print "P(Y=1 | Z=1,X=1) = "+str(Y1_Z1X1())
---
+++ ugm_joint_distribution.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+phi1 = {(0,0): 30, (0,1): 5, (1,0): 1, (1,1): 10}
+phi2 = {(0,0): 100, (0,1): 1, (1,0): 1, (1,1): 100}
+phi3 = {(0,0): 1, (0,1): 100, (1,0): 100, (1,1): 1}
+phi4 = {(0,0): 100, (0,1): 1, (1,0): 1, (1,1): 100}
+
+def factors(a,b,c,d):
+    return phi1[a,b] * phi2[b,c] * phi3[c,d] * phi4[d,a]
+
+def partition_function():
+    combinations = [list('{:04b}'.format(i)) for i in range(16)]
+    fact = [factors(*map(int,comb)) for comb in combinations]
+    return sum(fact)
+
+def p(a,b,c,d):
+    return float(factors(a,b,c,d)) / partition_function()
+
+if __name__ == '__main__':
+    print "Z = "+str(partition_function())
+    print "P(A=0,B=0,C=0,D=0) = "+str(p(0,0,0,0))
